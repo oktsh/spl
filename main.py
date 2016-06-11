a7c0da3ef8e598db300d7bf32c24f03984f312c7
Alexey <rapropp@gmail.com>
2016-06-11 12:53:10 +0300

    + row captions + patching week + reboot flag

--- main.py
+++ main.py
@@ -19,13 +19,20 @@ i = 0
 rw = xlwt.Workbook()
 rws = rw.add_sheet('servers')
 
+rws.write(i, 0, 'Server name')
+rws.write(i, 1, 'Patching date & time')
+rws.write(i, 2, 'Patching week')
+rws.write(i, 3, 'Reboot flag')
+
 # Создание словаря с парами сервер:дата патчинга
 for row in pslist:
-    psu = dict(server=row[0].upper(), date=row[3].upper())
+    psu = dict(server=row[0].upper(), date=row[3], week=row[2], reboot=row[4])
     if psu['server'] in servlist:
         i += 1
         rws.write(i, 0, psu['server'])
         rws.write(i, 1, psu['date'])
+        rws.write(i, 2, psu['week'])
+        rws.write(i, 3, psu['reboot'])
 
 # Сохранение файла с датой
 date = str(datetime.date.today())
--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from distutils.core import setup
 import py2exe
-setup(console=['D:\\Users\\40197341\\Data\\My projects\\PycharmProjects\\Server patching list\\main.py'])
+setup(console=['D:\\GitHub\\learn\\spl\\main.py'])
